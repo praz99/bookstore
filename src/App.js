9bc20bb30a8ebf2515915621df4fa89273b5ae3a
praz <t.prazwal@gmail.com>
2021-01-11 14:03:34 +0100

    change the home page content.

--- src/App.js
+++ src/App.js
@@ -3,20 +3,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
+          Welcome to my Bookstore.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
