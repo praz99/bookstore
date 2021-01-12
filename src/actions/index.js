390935e771a32596b10d6b23e4089179e2640b59
praz <t.prazwal@gmail.com>
2021-01-12 17:28:23 +0100

    add reducers and actions.

---
+++ src/actions/index.js
@@ -0,0 +1,18 @@
+const CREATE_BOOK = 'CREATE_BOOK';
+const REMOVE_BOOK = 'REMOVE_BOOK';
+
+const createBookAction = book => (
+  {
+    type: CREATE_BOOK,
+    book,
+  }
+);
+
+const removeBookAction = book => (
+  {
+    type: REMOVE_BOOK,
+    book,
+  }
+);
+
+export { CREATE_BOOK, REMOVE_BOOK, createBookAction, removeBookAction };
