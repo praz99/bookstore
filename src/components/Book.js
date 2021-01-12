537c34f73de759c51be9bcb13194de83e0922297
praz <t.prazwal@gmail.com>
2021-01-12 16:06:39 +0100

    implement Book component.

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,5 +1,23 @@
-const Book = () => {
+import React from 'react';
+import PropTypes from 'prop-types';
 
+const Book = book => {
+  const { id, title, category } = book;
+  return (
+    <tr>
+      <td>{id}</td>
+      <td>{title}</td>
+      <td>{category}</td>
+    </tr>
+  );
+};
+
+Book.propTypes = {
+  book: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    category: PropTypes.string.isRequired,
+  }),
 };
 
 export default Book;
