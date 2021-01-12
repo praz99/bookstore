5aaa912ff76a406dd1c369cceb899318afd30981
praz <t.prazwal@gmail.com>
2021-01-12 16:36:05 +0100

    add BookForm component.

--- src/containers/BooksForm.js
+++ src/containers/BooksForm.js
@@ -1,7 +1,25 @@
 import React from 'react';
 
 const BooksForm = () => {
+  const category = ['Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
 
+  return (
+    <>
+      <h2>Add New Book...</h2>
+      <label htmlFor="form-book-title">Title of the Book</label>
+      <input type="text" id="form-book-title" name="form-book-title" />
+      <label htmlFor="categories">Choose a category:</label>
+      <select id="categories">
+        <option value="action">Action</option>
+        <option value="biography">Biography</option>
+        <option value="history">History</option>
+        <option value="horror">Horror</option>
+        <option value="kids">Kids</option>
+        <option value="learning">Learning</option>
+        <option value="sci-fi">Sci-Fi</option>
+      </select>
+    </>
+  );
 };
 
 export default BooksForm;
