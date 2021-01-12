1039474a66c2d322392e95e15edcccd5a72d149b
praz <t.prazwal@gmail.com>
2021-01-12 15:50:16 +0100

    expand BookList component

--- src/containers/BooksList.js
+++ src/containers/BooksList.js
@@ -1,22 +1,35 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import Book from '../components/Book';
 
-const BooksList = () => (
-  <table className="books-table">
-    <thead>
-      <tr>
-        <th>Id</th>
-        <th>Title</th>
-        <th>Category</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <td>1234</td>
-        <td>ABC of DEF</td>
-        <td>Entertainment</td>
-      </tr>
-    </tbody>
-  </table>
+const BooksList = ({ books }) => {
+  const booksList = books.map(book => (<Book key={`book-${book.id}`} book={book} />));
+
+  return (
+    <table className="books-table">
+      <thead>
+        <tr>
+          <th>Id</th>
+          <th>Title</th>
+          <th>Category</th>
+        </tr>
+      </thead>
+      <tbody>
+        {booksList}
+      </tbody>
+    </table>
+  );
+};
+
+BooksList.propTypes = {
+  books: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
+
+const mapStateToProps = state => (
+  {
+    books: state.books
+  }
 );
 
-export default BooksList;
+export default connect(mapStateToProps, null)(BooksList);
