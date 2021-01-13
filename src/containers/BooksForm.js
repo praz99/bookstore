8043e3200ef4c23aef37feb2afd49eee3e023108
praz <t.prazwal@gmail.com>
2021-01-13 09:03:56 +0100

    fix eslint errors.

--- src/containers/BooksForm.js
+++ src/containers/BooksForm.js
@@ -1,23 +1,25 @@
 import React from 'react';
 
 const BooksForm = () => {
-  const category = ['Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
+  const categories = ['Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
 
   return (
     <form>
       <h2>Add New Book...</h2>
-      <label htmlFor="form-book-title">Title of the Book</label>
-      <input type="text" id="form-book-title" name="form-book-title" />
-      <label htmlFor="categories">Choose a category:</label>
-      <select id="categories">
-        <option value="action">Action</option>
-        <option value="biography">Biography</option>
-        <option value="history">History</option>
-        <option value="horror">Horror</option>
-        <option value="kids">Kids</option>
-        <option value="learning">Learning</option>
-        <option value="sci-fi">Sci-Fi</option>
-      </select>
+      <label htmlFor="form-book-title">
+        Title of the Book
+        <input type="text" id="form-book-title" name="form-book-title" />
+      </label>
+      <label htmlFor="categories">
+        Choose a category
+        <select id="categories">
+          {
+            categories.map(category => (
+              <option key={category} value={category}>{category}</option>
+            ))
+          }
+        </select>
+      </label>
       <button type="submit">Submit</button>
     </form>
   );
