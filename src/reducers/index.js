70fd9b37e46e96b5b8e31bacc111e7a1ff5d8a65
praz <t.prazwal@gmail.com>
2021-01-14 13:34:24 +0100

    add filter.js reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,11 @@
 import { createStore, combineReducers } from 'redux';
 import bookReducer from './books';
+import filterReducer from './filter';
 import INITIAL_STATE from '../constants/initialState';
 
 const rootReducer = combineReducers({
   books: bookReducer,
+  filter: filterReducer,
 });
 
 const store = createStore(rootReducer, INITIAL_STATE);
